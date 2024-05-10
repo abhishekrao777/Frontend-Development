3df5b1c805aaa9087a82d861050e85e636645fd6
abhishekkumar9935 <abhishekkumargautam7852@gmail.com>
2024-05-10 14:18:34 +0530

    project-2

---
+++ HTML-CSS/project-2/app.js
@@ -0,0 +1,22 @@
+// used to store keyed collections & complex entities property pair
+// object are a collection of properties
+
+// const item = {
+//     price:100.99,
+//     discount:50,
+//     colors:["red","pink"]
+// }
+
+// console.log(item.colors[0]);
+
+const classInfo = {
+    aman:{
+        grade:"A+",
+        city:"Delhi"
+    },
+    shradha:{
+        grade:"A",
+        city:"Pune"
+    }
+};
+console.log(classInfo);
